a6a1dcdc216f75b0f987f923b5dd0212c2938764
CreativeSTH <sth.frontend@gmail.com>
2025-02-02 22:39:39 -0500

    starting vanilla styles

--- src/app/home/Home.jsx
+++ src/app/home/Home.jsx
@@ -1,6 +1,9 @@
 import { useRef } from "react"
 import { useNavigate } from "react-router"
 import { useName } from "../../hooks/useName"
+import "../home/home.css"
+import pikachu from "/img/pikachu.webp"
+import pokemon from "/img/pokemon.svg"
 
 function Home() {
     const inputRef = useRef()
@@ -13,7 +16,9 @@ function Home() {
         navigate('/pokedex')
     }
   return (
-    <div>
+    <div className="container">
+      <img className="pikachu" src={pikachu} alt="imagen de charizard" />
+      <img className="pokelogo" src={pokemon} alt="imagen de charizard" />
       <h1>POKEDEX</h1>
       <h2>!Hola Entrenador</h2>
       <p>Para poder comenzar dame tu nombre</p>
